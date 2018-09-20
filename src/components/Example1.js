cfe8e32442bfec7850f0814df6fc29018af6d6a1
kaschief <kaschiefj@gmail.com>
2018-09-20 23:34:29 +0200

    exercise 1 completed

--- src/components/Example1.js
+++ src/components/Example1.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import GoogleMap from 'google-map-react';
-import CircleMarker from './markers/CircleMarker'
+import CircleMarker from './markers/CircleMarker';
 
 class Example1 extends Component {
   render() {
     return (
-      <div style={{ position: 'relative', width: '100%', height: 'calc(100vh - 40px)' }}>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: 'calc(100vh - 40px)'
+        }}
+      >
         <GoogleMap
           margin={[10, 20, 30, 40]}
-          // apiKey={YOUR_GOOGLE_MAP_API_KEY}
+          // apiKey={AIzaSyBG0SybP0EKWH3Jvwki7IR5AMyO_cUeeQc}
           center={{ lat: 52.506, lng: 13.37 }}
-          zoom={10}>
-
+          zoom={10}
+        >
           <CircleMarker
-            lat={52.5063688} lng={13.3711224}
+            lat={52.5063688}
+            lng={13.3711224}
             borderColor="red"
-            onClick={e => console.log("click")}
-            onMouseLeave={e => console.log("mouseLeave")}>
-            Ironhack Berlin
+            onClick={e => console.log('click')}
+            onMouseLeave={e => console.log('mouseLeave')}
+          >
+            Berlin
           </CircleMarker>
 
           <CircleMarker lat={48.8710895} lng={2.3070486} borderColor="blue">
-            Ironhack Paris
+            Paris
+          </CircleMarker>
+
+          <CircleMarker lat={40.3925321} lng={3.6982669} borderColor="green">
+            Madrid
           </CircleMarker>
         </GoogleMap>
       </div>
@@ -30,5 +42,3 @@ class Example1 extends Component {
 }
 
 export default Example1;
-
-
--- src/components/markers/CircleMarker.js
+++ src/components/markers/CircleMarker.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-const K_WIDTH = 100;
-const K_HEIGHT = 100;
+const K_WIDTH = 20;
+const K_HEIGHT = 20;
 
 const greatPlaceStyle = {
   // initially any map object has left top corner at lat lng coordinates
@@ -18,23 +18,49 @@ const greatPlaceStyle = {
   textAlign: 'center',
   border: '5px solid',
   borderRadius: K_HEIGHT,
-  backgroundColor: 'white',
+  // backgroundColor: 'white',
   color: '#3f51b5',
-  fontSize: 16,
+  fontSize: 7,
   fontWeight: 'bold',
   padding: 4
 };
 
 class CircleMarker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: true
+    };
+  }
+
+  changeColor = () => {
+    this.setState({
+      active: !this.state.active
+    });
+  };
+
   render() {
-    let borderColor = this.props.borderColor || '#f44336'
+    let borderColor = this.props.borderColor || '#f44336';
+    let backgroundColor;
+
+    if (this.state.active === true) {
+      backgroundColor = 'yellow';
+    } else backgroundColor = 'orange';
+
     return (
-      <div style={{ ...greatPlaceStyle, borderColor: borderColor }} onClick={this.props.onClick} onMouseLeave={this.props.onMouseLeave} >
+      <div
+        style={{
+          ...greatPlaceStyle,
+          borderColor: borderColor,
+          backgroundColor: backgroundColor
+        }}
+        onClick={e => this.changeColor()}
+        onMouseLeave={this.props.onMouseLeave}
+      >
         <div>{this.props.children}</div>
       </div>
     );
   }
 }
-
 
 export default CircleMarker;
